2f1a5068ecea118a207a1b98fcd5833f48d7627b
Encrux <boesch.daniel98@yahoo.com>
2020-02-22 22:26:21 +0100

    Add Player Keyboard Input, movement and Basic Map

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 direction;
+    [SerializeField] float moveSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += (Vector3) direction * moveSpeed;
+        Camera.main.transform.position = gameObject.transform.position + new Vector3(0,0,-10);
+    }
+
+    public void Walk(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+
+}
---
+++ Assets/Scripts/Player/PlayerKeyboardListener.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyboardListener : MonoBehaviour
+{
+    PlayerController playerController;
+    Vector2 direction;
+
+    void Start()
+    {
+        playerController = gameObject.GetComponent<PlayerController>();
+        direction = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction *= 0;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            Vertical();
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Horizonzal();
+        }
+
+        playerController.Walk(direction.normalized);
+    }
+
+    private void Vertical()
+    {
+        direction.y += Input.GetKey(KeyCode.W) ? 1 : 0;
+        direction.y -= Input.GetKey(KeyCode.S) ? 1 : 0;
+    }
+
+    private void Horizonzal()
+    {
+        direction.x += Input.GetKey(KeyCode.D) ? 1 : 0;
+        direction.x -= Input.GetKey(KeyCode.A) ? 1 : 0;
+    }
+
+}
